e63729f10283c1757d8c741d249ce3ce80a1cabb
John Handley <jwhandley17@gmail.com>
2023-10-14 17:27:24 +0100

    Upgrade data structures

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
+use std::collections::HashMap;
 use std::path::PathBuf;
-use rand::prelude::*;
 use std::time::Instant;
+use rand::prelude::*;
 use indicatif::{ProgressBar, ProgressStyle};
+use ndarray::Array2;
 
 mod wordio;
+use wordio::ScoreMap;
 
-fn calculate_score(guess: &str, secret: &str) -> i32 {
-    let mut score = [0; 5];
+
+fn calculate_score(guess: &str, secret: &str) -> u8 {
+    let mut score: [u8; 5] = [0; 5];
     let guess_chars: Vec<char> = guess.chars().collect();
     let secret_chars: Vec<char> = secret.chars().collect();
     let mut used_secret_indices = [false; 5];
@@ -32,7 +36,7 @@ fn calculate_score(guess: &str, secret: &str) -> i32 {
     // Convert to base 3
     let mut result = 0;
     for (i, v) in score.iter().enumerate() {
-        result += i32::pow(3, i as u32) * v;
+        result += u8::pow(3, i as u32) * *v;
     }
 
     result
@@ -49,47 +53,96 @@ fn score_to_array(score: &i32) -> [i32; 5] {
     result
 }
 
-fn reduce_allowed_words(allowed_words: &Vec<String>, guess: &str, score: &i32) -> Vec<String> {
-    let mut result = Vec::new();
-    for word in allowed_words {
-        if calculate_score(guess, word) == *score {
-            result.push(word.to_owned());
+fn precalculate_scores(allowed_words: &HashMap<String, usize>) -> ScoreMap {
+    let pb = ProgressBar::new(allowed_words.len() as u64 * allowed_words.len() as u64);
+    pb.set_style(ProgressStyle::default_bar()
+        .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}")
+        .expect("Failed to set progress bar style")
+        .progress_chars("##-"));
+
+    let mut score_map = Array2::<u8>::zeros((allowed_words.len(), allowed_words.len()));
+
+    for (guess, guess_idx) in allowed_words.iter() {
+        for (secret, secret_idx) in allowed_words.iter() {
+            score_map[(*guess_idx, *secret_idx)] = calculate_score(guess, secret);
+            pb.inc(1);
         }
     }
-    result
+    pb.finish();
+
+    ScoreMap { map: score_map }
 }
 
-fn simulate_game(allowed_words: &Vec<String>, secret: &str) -> i32 {
+fn best_guess(allowed_words: &HashMap<String, usize>, _score_map: &ScoreMap) -> (String, usize) {
+    // Choose the guess that reduces the option set the most
     let mut rng = rand::thread_rng();
-    let mut guess = allowed_words.choose(&mut rng).unwrap().to_owned();
-    let mut score = calculate_score(&guess, secret);
 
-    let mut allowed_words = allowed_words.clone();
-    let mut final_score = 1;
+    let (guess, guess_idx) = allowed_words.iter().choose(&mut rng).unwrap();
 
-    while score != 242 {
-        final_score += 1;
-        allowed_words = reduce_allowed_words(&allowed_words, &guess, &score);
-        guess = allowed_words.choose(&mut rng).unwrap().to_owned();
-        score = calculate_score(&guess, secret);
+    (guess.clone(), *guess_idx)
+}
 
-        if final_score > 5 {
-            break;
+fn reduce_allowed_words(allowed_words: &HashMap<String, usize>, guess_idx: usize, score: u8, score_map: &ScoreMap) -> HashMap<String, usize> {
+    let mut new_allowed_words = allowed_words.clone();
+
+    for possible_secret in allowed_words.clone() {
+        if score_map.map[(guess_idx,possible_secret.1)] != score {
+            new_allowed_words.remove(&possible_secret.0);
         }
-
     }
 
-    final_score
+    new_allowed_words
+    
+}
+
+fn simulate_game(allowed_words: &HashMap<String, usize>, secret: &str, score_map: &ScoreMap) -> (bool, i32) {    
+    let mut is_solved = false;
+    let mut allowed_words = allowed_words.clone();
+    let mut final_score = 0;
+    let secret_idx = *allowed_words.get(secret).unwrap();
+
+    while !is_solved {
+        let (_guess, guess_idx) = best_guess(&allowed_words, score_map);
+        let score = score_map.map[(guess_idx,secret_idx)];
+
+
+
+        allowed_words = reduce_allowed_words(&allowed_words, guess_idx, score, score_map);
+
+        
+        final_score += 1;
+
+        if score == 242 {
+            is_solved = true;
+        } else if final_score > 5 {
+            break;
+        }
+    }
+
+    (is_solved, final_score)
 }
 
 
 
 fn main() {
-    let possible_words: Vec<String> = wordio::read_words(PathBuf::from("data/possible_words.txt"));
-    let allowed_words: Vec<String> = wordio::read_words(PathBuf::from("data/allowed_words.txt"));
+    let possible_words: HashMap<String, usize> = wordio::read_words(PathBuf::from("data/possible_words.txt"));
+    let allowed_words: HashMap<String, usize> = wordio::read_words(PathBuf::from("data/allowed_words.txt"));
+    
+    println!("Loading score map...");
+    let score_map = match wordio::load_score_map() {
+        Some(score_map) => score_map,
+        None => {
+            println!("No score map found, precalculating...");
+            let score_map = precalculate_scores(&allowed_words);
+            println!("Saving score map...");
+            wordio::save_score_map(&score_map);
+            
+            score_map
+        }
+    };
+    println!("Done!");
 
-    // let secret = possible_words.choose(&mut rand::thread_rng()).unwrap().to_owned();
-    // println!("The secret word is: {}", secret);
+    println!("Running simulations!");
     let n_simulations = 1;
 
     let pb = ProgressBar::new(n_simulations*possible_words.len() as u64);
@@ -100,10 +153,16 @@ fn main() {
 
     let mut scores = Vec::new();
     let mut times = Vec::new();
-    for secret in &possible_words {
+    let mut wins = 0;
+    for secret in possible_words.keys() {
         for _ in 0..n_simulations {
             let start_time = Instant::now();
-            let score = simulate_game(&allowed_words, &secret);
+            let (solved, score)= simulate_game(&allowed_words, secret, &score_map);
+
+            if solved {
+                wins += 1;
+            }
+
             scores.push(score);
             times.push(start_time.elapsed().as_millis());
             pb.inc(1);
@@ -112,10 +171,13 @@ fn main() {
     
     pb.finish();
 
-    let average_score = scores.iter().sum::<i32>() as f32 / n_simulations as f32;
+    let average_score = scores.iter().sum::<i32>() as f32 / n_simulations as f32 / possible_words.len() as f32;
     println!("Average score: {}", average_score);
 
-    let average_time = times.iter().sum::<u128>() as f32 / n_simulations as f32;
+    let win_rate = wins as f32 / n_simulations as f32 / possible_words.len() as f32;
+    println!("Win rate: {}", win_rate);
+
+    let average_time = times.iter().sum::<u128>() as f32 / n_simulations as f32 / possible_words.len() as f32;
     println!("Average time: {} ms", average_time);
 
     let total_time = times.iter().sum::<u128>() / 1000;
--- src/wordio.rs
+++ src/wordio.rs
@@ -1,17 +1,57 @@
-use std::io::{BufRead, BufReader};
-use std::fs::File;
+use std::collections::HashMap;
+use std::fs::{File, OpenOptions};
+use std::io::{Read, Write, BufRead, BufReader};
 use std::path::PathBuf;
+use serde::{Deserialize, Serialize};
+use ndarray::Array2;
 
-pub fn read_words(path: PathBuf) -> Vec<String> {
-    let mut words = Vec::new();
+#[derive(Serialize, Deserialize)]
+pub struct ScoreMap {
+    pub map: Array2<u8>,
+}
+
+pub fn read_words(path: PathBuf) -> HashMap<String, usize> {
+    let mut words = HashMap::new();
     let file = File::open(path).expect("Failed to open file");
     let reader = BufReader::new(file);
 
-    for line in reader.lines() {
+    for (i, line) in reader.lines().enumerate() {
         let line = line.expect("Failed to read line").trim().to_lowercase();
-        words.push(line);
+        words.insert(line, i);
     }
-    
 
     words
+}
+
+pub fn load_score_map() -> Option<ScoreMap> {
+    let mut file = match File::open("data/score_map.bincode") {
+        Ok(file) => file,
+        Err(_) => return None,
+    };
+
+    let mut encoded = Vec::new();
+    file.read_to_end(&mut encoded)
+        .expect("Failed to read score_map.mat");
+
+    let score_map: ScoreMap = bincode::deserialize(&encoded)
+        .expect("Failed to deserialize score map");
+
+    Some(score_map)
+
+
+}
+
+pub fn save_score_map(score_map: &ScoreMap) {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open("data/score_map.bincode")
+        .expect("Failed to open score_map.mat");
+
+    let encoded = bincode::serialize(score_map)
+        .expect("Failed to serialize score map");
+
+    file.write_all(&encoded)
+        .expect("Failed to write score_map.mat");
 }
